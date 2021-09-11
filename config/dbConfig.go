a3f544d766c5ff25debd6128d2f6438e95f03b0c
Mada Satya Bayu Ambika <madasatya6@gmail.com>
2021-09-11 14:41:51 +0700

    update

--- config/dbConfig.go
+++ config/dbConfig.go
@@ -7,7 +7,9 @@ import (
 
 
 func GetConnection() (*sql.DB,error){
-	db,err := sql.Open("mysql","user:pass@tcp(xxx.xxx.xxx.xxx:PORT)/namedb")
+	var dsn = fmt.Sprintf("%s:%s@/%s", "root", "", "golang-mysql-graphql")
+	//db,err := sql.Open("mysql","user:pass@tcp(xxx.xxx.xxx.xxx:PORT)/namedb")
+	db,err := sql.Open("mysql",dsn)
 	if err != nil {
 		panic(err.Error())
 	}
---
+++ golang-mysql-graphql.sql
@@ -0,0 +1,60 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Sep 2021 pada 09.41
+-- Versi server: 10.4.11-MariaDB
+-- Versi PHP: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `golang-mysql-graphql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `products`
+--
+
+CREATE TABLE `products` (
+  `ID_PRO` varchar(16) NOT NULL,
+  `PRO_NAME` varchar(50) DEFAULT NULL,
+  `QTE_IN_STOCK` int(11) DEFAULT NULL,
+  `PRO_IMG` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `products`
+--
+
+INSERT INTO `products` (`ID_PRO`, `PRO_NAME`, `QTE_IN_STOCK`, `PRO_IMG`) VALUES
+('lagu', 'ketan', 16, 'aslkfl'),
+('pisang12', 'pisang bakar', 12, 'balisojdoeidjdl'),
+('pisang13', 'sebakul nasi', 14, 'aslkdlfl');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ID_PRO`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
